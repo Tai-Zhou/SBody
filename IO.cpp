d8910d49cc23aaf3f7597f036cf82d3698d3ff23
Tai Zhou <21986946+Tai-Zhou@users.noreply.github.com>
2021-05-13 11:07:10 +0800

    Fix undefined reference

--- IO.cpp
+++ IO.cpp
@@ -2,34 +2,40 @@
 
 #include <fitsio.h>
 
+using namespace std;
+
 namespace SBody {
 	namespace IO {
 		template <typename T>
-		int NumPySave(const std::vector<std::vector<T>> &data, std::string fileName) {
+		int NumPySave(const vector<vector<T>> &data, string fileName) {
 			char NumPyHead[10] = {'\x93', 'N', 'U', 'M', 'P', 'Y', '\x01', '\x00', '\x76', '\x00'};
-			std::ofstream NumPyFile(fileName + ".npy", std::ios::binary | std::ios::out);
+			ofstream NumPyFile(fileName + ".npy", ios::binary | ios::out);
 			NumPyFile.write(NumPyHead, 10);
 			NumPyFile << "{'descr': '<" << (sizeof(T) == 4 ? "i4" : "f8")
 					  << "', 'fortran_order': False, 'shape': (                                                                  \n";
-			for (const std::vector<T> &line : data)
+			for (const vector<T> &line : data)
 				NumPyFile.write((char *)(&line[0]), sizeof(T) * line.size());
 			NumPyFile.seekp(61);
 			NumPyFile << data.size() << ", " << data[0].size() << "), }";
 			NumPyFile.close();
 			return 0;
 		}
+		template int NumPySave(const vector<vector<double>> &data, string fileName);
+		template int NumPySave(const vector<vector<int>> &data, string fileName);
 		template <typename T>
-		int CSVSave(const std::vector<std::vector<T>> &data, std::string fileName) {
-			std::ofstream CSVFile(fileName + ".csv", std::ios::out);
-			for (const std::vector<T> &line : data) {
+		int CSVSave(const vector<vector<T>> &data, string fileName) {
+			ofstream CSVFile(fileName + ".csv", ios::out);
+			for (const vector<T> &line : data) {
 				for (const T element : line)
 					CSVFile << element << ',';
-				CSVFile.seekp(-1, std::ios::cur);
-				CSVFile << std::endl;
+				CSVFile.seekp(-1, ios::cur);
+				CSVFile << endl;
 			}
 			CSVFile.close();
 			return 0;
 		}
+		template int CSVSave(const vector<vector<double>> &data, string fileName);
+		template int CSVSave(const vector<vector<int>> &data, string fileName);
 		int save() {
 			return 0;
 		}
