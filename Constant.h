bd0b032ac7f36b8c189757202ff6f20f60bd9511
Tai Zhou <21986946+Tai-Zhou@users.noreply.github.com>
2022-02-17 20:47:49 +0800

    Update constants

--- Constant.h
+++ Constant.h
@@ -7,18 +7,18 @@ namespace SBody {
 		constexpr double e = 1.60217662e-19;
 		constexpr double h = 7.52764601e-76;
 		// Time
-		constexpr double s = 2.0302544672808e5;
-		constexpr double day = 1.75413986e10;
-		constexpr double yr = 6.40699584e12;
+		constexpr double s = 2.0302544672808357e5;
+		constexpr double day = 1.754139859730642e10;
+		constexpr double yr = 6.40699583766617e12;
 		// Length
-		constexpr double cm = 6.77219994e-6;
-		constexpr double R_sun = 4.71141950e5;
-		constexpr double AU = 1.01310669e8;
-		constexpr double mpc = 2.08968255e10;
-		constexpr double pc = 2.08968255e13;
-		constexpr double kpc = 2.08968255e16;
-		constexpr double Mpc = 2.08968255e19;
-		constexpr double Gpc = 2.08968255e22;
+		constexpr double cm = 6.772199944005381e-06;
+		constexpr double R_sun = 4.7114195010445436e5;
+		constexpr double AU = 1.0131066915778643e8;
+		constexpr double mpc = 2.0896825544594498e10;
+		constexpr double pc = 2.0896825544594498e13;
+		constexpr double kpc = 2.0896825544594498e16;
+		constexpr double Mpc = 2.0896825544594498e19;
+		constexpr double Gpc = 2.0896825544594498e22;
 		// Mass
 		constexpr double M_earth = 3.00348935e-6;
 		constexpr double M_jup = 9.5459e-4;
